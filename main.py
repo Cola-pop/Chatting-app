6e2c7bca1d554b00da41c58491309881473eacd2
Hrag Hayrabedian <hrag.h@hotmail.com>
2019-04-22 13:54:04 +0300

    add sessions

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ socketio = SocketIO(app)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
+
+@app.route('/')
+def sessions():
+    return render_template('index.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('Message Received')
+
+@socketio.on('my event')
+def handle_my_event(json, methods=['GET', 'POST']):
+    print('Received my event: ' + str(json))
+    socketio.emit('My Response', json, callback=messageReceived)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
