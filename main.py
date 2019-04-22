e5935ae3b2c6e29a819c5cd6d92e291e7327130b
Hrag Hayrabedian <hrag.h@hotmail.com>
2019-04-22 14:11:29 +0300

    modify main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dnuif32nbd#$'
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
 if __name__ == '__main__':
@@ -10,15 +10,15 @@ if __name__ == '__main__':
 
 @app.route('/')
 def sessions():
-    return render_template('index.html')
+    return render_template('session.html')
 
 def messageReceived(methods=['GET', 'POST']):
-    print('Message Received')
+    print('message was received!!!')
 
 @socketio.on('my event')
-def handle_my_event(json, methods=['GET', 'POST']):
-    print('Received my event: ' + str(json))
-    socketio.emit('My Response', json, callback=messageReceived)
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
